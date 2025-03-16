046e4acb360cb7267f77e7eff1ed52a6a3679d48
Oleksandr Kuzminskyi <1763754+akuzminsky@users.noreply.github.com>
2025-03-16 08:20:17 -0700

    Fix ec2_client call (#14)
    * Fix ec2_client call
    
    Call the public property ec2_client to ensure it's initialized whether
    passed to the EC2Instance class or not.
    
    * Bump version: 0.7.0 → 0.7.1

--- tests/aws/ec2_instance/test_describe_instance.py
+++ tests/aws/ec2_instance/test_describe_instance.py
@@ -1,11 +1,16 @@
-from unittest.mock import Mock
+from unittest import mock
 
 from infrahouse_core.aws.ec2_instance import EC2Instance
 
 
-def test_ec2_client():
+def test_describe_instance():
+    with mock.patch("infrahouse_core.aws.ec2_instance.get_client") as mock_client:
+        assert EC2Instance(instance_id="foo")._describe_instance
+        mock_client.assert_called_once_with("ec2", region=None)
 
-    mock_client = Mock()
+
+def test_describe_instance_with_client():
+    mock_client = mock.Mock()
     mock_client.describe_instances.return_value = {
         "Reservations": [
             {
@@ -20,6 +25,5 @@ def test_ec2_client():
         ],
     }
 
-    instance = EC2Instance("foo", ec2_client=mock_client)
-    assert instance.tags == {"Name": "foo-instance"}
+    assert EC2Instance(instance_id="foo", ec2_client=mock_client)._describe_instance
     mock_client.describe_instances.assert_called_once_with(InstanceIds=["foo"])
